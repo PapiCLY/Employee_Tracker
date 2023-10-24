29e9aebe60bcba3a33aac237399d06ba513b7311
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-10-24 02:36:41 -0400

    sql tables error

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,30 @@
-drop db if exists employee_tracker_db;
-create db employee_tracker_db;
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 create TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT PRIMARY KEY auto_increment,
+    name VARCHAR(30) not null
 );
 
 create TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY auto_increment,
     title VARCHAR(30),
     salary DECIMAL(10,2),
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
+    ON DELETE CASCADE
 );
 
 create TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY auto_increment,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
+    ON DELETE CASCADE
 );
 
 -- Path: db/seeds.sql
