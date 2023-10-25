8fb98496e24d3cf4911028a26fbefd7f51f8992e
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-10-24 20:10:32 -0400

    updated employee list

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,32 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
+
 USE employee_tracker_db;
 
-create TABLE department (
-    id INT PRIMARY KEY auto_increment,
-    name VARCHAR(30) not null
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
 );
 
-create TABLE role (
-    id INT PRIMARY KEY auto_increment,
-    title VARCHAR(30),
-    salary DECIMAL(10,2),
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
-    ON DELETE CASCADE
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-create TABLE employee (
-    id INT PRIMARY KEY auto_increment,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
-    ON DELETE CASCADE
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
 -- Path: db/seeds.sql
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,33 @@
-INSERT INTO employee_tracker_db.department (name) VALUES
+use employee_tracker_db;
+
+INSERT INTO department
+    (name)
+VALUES
     ('Sales'),
     ('Engineering'),
     ('Finance'),
     ('Legal');
 
-INSERT INTO employee_tracker_db.role (title, salary, department_id) VALUES
-    ('Sales Lead', 100000.00, 1),
-    ('Salesperson', 80000.00, 1),
-    ('Lead Engineer', 150000.00, 2),
-    ('Software Engineer', 120000.00, 2),
-    ('Accountant', 125000.00, 3),
-    ('Legal Team Lead', 250000.00, 4),
-    ('Lawyer', 190000.00, 4);
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
 
-INSERT INTO employee_tracker_db.employee (first_name, last_name, role_id, manager_id) VALUES
+CREATE TABLE employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
     ('John', 'Doe', 1, NULL),
     ('Mike', 'Chan', 2, 1),
     ('Ashley', 'Rodriguez', 3, NULL),
     ('Kevin', 'Tupik', 4, 3),
-    ('Malia', 'Brown', 5, NULL),
-    ('Sarah', 'Lourd', 6, 5),
-    ('Tom', 'Allen', 7, NULL),
-    ('Tina', 'Lee', 8, 7);
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
